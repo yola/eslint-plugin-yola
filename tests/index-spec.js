5d440c2039664899af7fbe704a199f07ee29acbd
Rusinov Anton <rusinovantondev@gmail.com>
2017-11-06 00:27:25 +0200

    Simplify plugin test snapshots
    to get use of snapshot testing need to make it more robust
    - omit rules details by shallowing rules objects to immediate child
    - omit variable 'parser' path

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -1,6 +1,27 @@
 const plugin = require('../index');
 
-test('Plugin must contain configs and rules properties', () => {
-  expect(plugin.configs).toMatchSnapshot();
-  expect(plugin.rules).toMatchSnapshot();
+const makeShallow = (obj) => {
+  const o = Object.assign({}, obj);
+  Object.keys(o).forEach((key) => {
+    o[key] = true;
+  });
+
+  return o;
+};
+
+describe('eslint-plugin-yola', () => {
+  it('must contain configs and rules properties', () => {
+    const p = Object.assign({}, plugin);
+
+    p.rules = makeShallow(p.rules);
+    Object.keys(p.configs)
+      .forEach((key) => {
+        if (p.configs[key].parser) {
+          p.configs[key].parser = true;
+        }
+        p.configs[key].rules = makeShallow(p.configs[key].rules);
+      });
+    expect(p.configs).toMatchSnapshot();
+    expect(p.rules).toMatchSnapshot();
+  });
 });
