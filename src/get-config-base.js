53aa3b3ad76fa7b056481dd1b12d8e5c5dedea9f
Rusinov Anton <rusinovantondev@gmail.com>
2017-11-21 23:01:53 +0200

    Extend base configuration
    At this moment .eslintrc.js is tiny and doesn't covers needs of repos it
    used to lint, so to prevent from overriding base configuration files for every repo:
    
    - add proper env to prevent invalid errors about global variables
    - override `import/no-extraneous-dependencies` rule for test files that should use devDependencies

--- src/get-config-base.js
+++ src/get-config-base.js
@@ -1,4 +1,30 @@
 module.exports = () => ({
   parser: require.resolve('babel-eslint'),
   plugins: ['yola'],
+  env: {
+    browser: true,
+  },
+  overrides: [
+    {
+      files: [
+        '*-spec.js',
+        '*-spec.jsx',
+        '*.spec.js',
+        '*.spec.jsx',
+      ],
+      env: {
+        mocha: true,
+      },
+      rules: {
+        // Override `import/no-extraneous-dependencies` rule for test files
+        // that should use devDependencies
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            'devDependencies': true,
+          },
+        ],
+      },
+    },
+  ],
 });
