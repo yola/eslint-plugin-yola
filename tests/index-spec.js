f87a9dc33016619b119a6ca4f2d6b3fca4a261de
Rusinov Anton <rusinovantondev@gmail.com>
2017-11-22 22:47:29 +0200

    Replace invalid snapshot testing
    Snapshot were initially used for development phase refactoring.
    It is not maintainable to keep snapshot of that large object with
    non-guaranteed keys order and env dependent values.
    
    Replacing it by simple but robust high-order prop check.

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -1,27 +1,9 @@
 const plugin = require('../index');
 
-const makeShallow = (obj) => {
-  const o = Object.assign({}, obj);
-  Object.keys(o).forEach((key) => {
-    o[key] = true;
-  });
-
-  return o;
-};
-
 describe('eslint-plugin-yola', () => {
   it('must contain configs and rules properties', () => {
-    const p = Object.assign({}, plugin);
-
-    p.rules = makeShallow(p.rules);
-    Object.keys(p.configs)
-      .forEach((key) => {
-        if (p.configs[key].parser) {
-          p.configs[key].parser = true;
-        }
-        p.configs[key].rules = makeShallow(p.configs[key].rules);
-      });
-    expect(p.configs).toMatchSnapshot();
-    expect(p.rules).toMatchSnapshot();
+    expect(plugin).toHaveProperty('configs.base.rules');
+    expect(plugin).toHaveProperty('configs.react.rules');
+    expect(plugin).toHaveProperty('rules');
   });
 });
