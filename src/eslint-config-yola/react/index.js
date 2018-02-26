090948712d13316f96b870989eeea6924c26e52e
Rusinov Anton <rusinovantondev@gmail.com>
2018-02-26 16:21:56 +0200

    prettify source

--- src/eslint-config-yola/react/index.js
+++ src/eslint-config-yola/react/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: [
-    '../base',
-    '../base/rules/strict',
-    './rules/react',
-    './rules/react-a11y',
+    "../base",
+    "../base/rules/strict",
+    "./rules/react",
+    "./rules/react-a11y"
   ].map(require.resolve),
-  rules: {},
+  rules: {}
 };
--- src/get-config-base.js
+++ src/get-config-base.js
@@ -1,41 +1,34 @@
 module.exports = () => ({
-  parser: require.resolve('babel-eslint'),
-  plugins: ['yola'],
+  parser: require.resolve("babel-eslint"),
+  plugins: ["yola"],
   env: {
-    browser: true,
+    browser: true
   },
   rules: {
-    'no-console': [
-      'error',
+    "no-console": [
+      "error",
       {
-        allow: [
-          'error',
-        ],
-      },
-    ],
+        allow: ["error"]
+      }
+    ]
   },
   overrides: [
     {
-      files: [
-        '**/*-spec.js',
-        '**/*-spec.jsx',
-        '**/*.spec.js',
-        '**/*.spec.jsx',
-      ],
+      files: ["**/*-spec.js", "**/*-spec.jsx", "**/*.spec.js", "**/*.spec.jsx"],
       env: {
         mocha: true,
-        jest: true,
+        jest: true
       },
       rules: {
         // Override `yola/import/no-extraneous-dependencies` rule for test files
         // that should use devDependencies
-        'yola/import/no-extraneous-dependencies': [
-          'error',
+        "yola/import/no-extraneous-dependencies": [
+          "error",
           {
-            devDependencies: true,
-          },
-        ],
-      },
-    },
-  ],
+            devDependencies: true
+          }
+        ]
+      }
+    }
+  ]
 });
