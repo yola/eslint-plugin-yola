61e1254253305cbb668e779f56f6b273158c6e20
Mykhailo Aleksiuk <mykhailo.aleksiuk@yola.com>
2023-06-26 18:35:19 +0300

    Disable no-unsafe-return & no-unsafe-call for typescript config.

--- src/eslint-config-yola/base/rules/style.js
+++ src/eslint-config-yola/base/rules/style.js
@@ -214,7 +214,7 @@ module.exports = {
 
     // require or disallow an empty line between class members
     // https://eslint.org/docs/rules/lines-between-class-members
-    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: false }],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
 
     // enforces empty lines around comments
     'lines-around-comment': 'off',
